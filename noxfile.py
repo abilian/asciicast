bd48bd2da46475d5243112abccb73fe3c20ab3d9
Stefane Fermigier <sf@fermigier.com>
2023-04-03 19:19:21 +0200

    chore: update cruft from cookiecutter template.

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,11 @@
 import nox
 
 PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
+DEFAULT_PYTHON_VERSION = PYTHON_VERSIONS[-1]
 nox.options.sessions = ["lint", "pytest"]
 
 
-@nox.session(python=PYTHON_VERSIONS)
+@nox.session(python=DEFAULT_PYTHON_VERSION)
 def lint(session: nox.Session) -> None:
     session.install("-e", ".[dev]")
     session.run("pip", "check")
